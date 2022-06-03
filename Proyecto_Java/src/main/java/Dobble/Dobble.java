f018f298f8722480f7e7cf7e46543f19d45c323d
Alan Donoso <Alan.Donoso.V@usach.cl>
2022-06-02 23:40:55 -0400

    TDA Dobble getters, setters y tostring

--- Proyecto_Java/src/main/java/Dobble/Dobble.java
+++ Proyecto_Java/src/main/java/Dobble/Dobble.java
@@ -1,17 +1,55 @@
 package Dobble;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Dobble {
 
     // Atributos
-    int maxC;
-    int orden;
-    List<String> Elements = new ArrayList<String>();
+    private int maxC;
+    private int orden;
+    private List elements;
+
+    public Dobble(int maxC, int orden, List elements) {
+        this.maxC = maxC;
+        this.orden = orden;
+        this.elements = elements;
+    }
+
+    public int getMaxC() {
+        return maxC;
+    }
+
+    public void setMaxC(int maxC) {
+        this.maxC = maxC;
+    }
+
+    public int getOrden() {
+        return orden;
+    }
+
+    public void setOrden(int orden) {
+        this.orden = orden;
+    }
+
+    public List getElements() {
+        return elements;
+    }
+
+    public void setElements(List elements) {
+        this.elements = elements;
+    }
+
+    @Override
+    public String toString() {
+        return "Dobble{" +
+                "maxC=" + maxC +
+                ", orden=" + orden +
+                ", elements=" + elements +
+                '}';
+    }
 
     // Metodos
-    public void GenerarMazo(String elements, int orden, int maxC){
+    public void GenerarMazo(List elements, int orden, int maxC){
 
         for(int i = 1; i <= orden+1; i = i + 1){
 
